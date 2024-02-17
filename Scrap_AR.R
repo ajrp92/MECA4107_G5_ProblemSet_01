cbab758e134807a8b2dec67072500ccef37cc513
ajrp92 <aj.rodriguezp1@uniandes.edu.co>
2024-02-16 21:35:52 -0500

    Create Scrap_AR.R
    Cree script para scrap la web.

---
+++ Scrap_AR.R
@@ -0,0 +1,20 @@
+## Parte 1 ## Web scrap ##
+##Work directory##
+
+setwd("C:\\Users\\AlfredoRP\\OneDrive - INALDE Business School - Universidad de La Sabana\\Attachments\\Economia\\ML\\Problem_set_1_Equipo_5\\scripts")
+
+##use rvest##
+library("tidyverse")
+library("rvest")
+
+##Web page""
+web <- "https://ignaciomsarmiento.github.io/GEIH2018_sample/"
+
+htlm <- read_html("web") 
+
+##links##
+
+links <- htlm %>% 
+  html_nodes("a") %>%
+  html_attr("href")
+ #
